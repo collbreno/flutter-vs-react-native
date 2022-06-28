0b0a99b75e7544ef474d0a3610e76f223d209163
Breno Coll <collbreno@dcc.ufrj.br>
2022-06-28 00:10:00 -0300

    Splitting tester into RNTester and FlutterTester

--- scripts/main.py
+++ scripts/main.py
@@ -2,7 +2,7 @@ import json
 import sys
 
 from gfxinfo_parser import GFXInfoParser
-from tester import Tester
+from tester import RNTester, FlutterTester
 
 def get_file_name():
     args = sys.argv
@@ -10,11 +10,27 @@ def get_file_name():
         raise Exception('Config file name not defined')
     return args[1]
 
+def get_framework():
+    args = sys.argv
+    if len(args) < 3:
+        raise Exception('Framework not defined')
+    return args[2]
+
 def read_json():
     file = open(get_file_name())
     data = json.load(file)
     return data
 
-if __name__ == '__main__':
+def create_tester():
     data = read_json()
-    Tester(data).test()
+    framework = get_framework()
+    if framework == 'rn':
+        return RNTester(data)
+    elif framework == 'flutter':
+        return FlutterTester(data)
+    else:
+        raise Exception("Framework must be rn or flutter")
+
+if __name__ == '__main__':
+    tester = create_tester()
+    tester.run()
--- scripts/tester.py
+++ scripts/tester.py
@@ -1,12 +1,14 @@
 from time import sleep
 from gfxinfo_parser import GFXInfoParser
 from utils import syscall
+import abc
 
 
-class Tester:
+class Tester(metaclass=abc.ABCMeta):
+    app_id: str
+
     def __init__(self, config) -> None:
         self.config = config
-        self.app_id = f'com.rn.{config["app"]}'
         self.parser = GFXInfoParser()
 
     def open_app(self):
@@ -14,23 +16,47 @@ class Tester:
         syscall(f'adb shell monkey -p {self.app_id} 1')
         sleep(1)
 
-    def reset_frames(self):
-        print('Resetting frames...')
-        syscall(f'adb shell dumpsys gfxinfo {self.app_id} reset')
-
+    @abc.abstractmethod
     def read_frames(self):
-        print('Reading app...')
-        res = syscall(f'adb shell dumpsys gfxinfo {self.app_id}')
-        parsed = self.parser.parse_histogram(res.stdout)
-        print(parsed)
+        raise NotImplementedError
 
     def execute_commands(self):
         print('Executing commands...')
         for cmd in self.config['commands']:
             syscall(cmd)
 
-    def test(self):
+    @abc.abstractmethod
+    def on_app_opened(self):
+        raise NotImplementedError
+
+    def run(self):
         self.open_app()
-        self.reset_frames()
+        self.on_app_opened()
         self.execute_commands()
         self.read_frames()
+
+class RNTester(Tester):
+    def __init__(self, config) -> None:
+        super().__init__(config)
+        self.app_id = f'com.rn.{config["app"]}'
+
+    def on_app_opened(self):
+        print('Resetting frames...')
+        syscall(f'adb shell dumpsys gfxinfo {self.app_id} reset')
+
+    def read_frames(self):
+        print('Reading frames...')
+        res = syscall(f'adb shell dumpsys gfxinfo {self.app_id}')
+        parsed = self.parser.parse_histogram(res.stdout)
+        print(parsed)
+
+class FlutterTester(Tester):
+    def __init__(self, config) -> None:
+        super().__init__(config)
+        self.app_id = f'com.flutter.{config["app"]}'
+
+    def on_app_opened(self):
+        pass
+
+    def read_frames(self):
+        pass #TODO: IMPLEMENT
