3a261336f3a81a6898c210f7401da23740d24b6f
Breno Coll <collbreno@dcc.ufrj.br>
2022-07-18 23:27:26 -0300

    [Flutter] [Stopwatch] Creating app

---
+++ flutter_stopwatch/lib/main.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Counter")),
+        body: const Center(
+          child: Counter(milliseconds: 16),
+        ),
+      ),
+    );
+  }
+}
+
+class Counter extends StatefulWidget {
+  final int milliseconds;
+
+  const Counter({Key? key, required this.milliseconds}) : super(key: key);
+
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  late int counter;
+
+  @override
+  void initState() {
+    super.initState();
+    counter = 0;
+    Timer.periodic(Duration(milliseconds: widget.milliseconds), (timer) {
+      setState(() {
+        counter += 1;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(counter.toString());
+  }
+}
