002cf128fabd64b089b206aa6fe8a327dbaefbfd
Breno Coll <collbreno@dcc.ufrj.br>
2022-07-18 23:48:03 -0300

    [RN] [Flutter] [Stopwatch] Standardizing apps

--- flutter_stopwatch/lib/main.dart
+++ flutter_stopwatch/lib/main.dart
@@ -16,15 +16,25 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(title: const Text("Counter")),
-        body: const Center(
-          child: Counter(milliseconds: 16),
-        ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Counter")),
+      body: const Center(
+        child: Counter(milliseconds: 16),
       ),
     );
   }
 }
+
 
 class Counter extends StatefulWidget {
   final int milliseconds;
--- rn_stopwatch/App.js
+++ rn_stopwatch/App.js
@@ -6,62 +6,76 @@
  * @flow strict-local
  */
 
- import React from 'react';
- import {
-   View,
-   Text,
-   StyleSheet,
- } from 'react-native';
- import {
-   Provider as PaperProvider,
-   Appbar,
-   FAB,
-   Portal,
- } from 'react-native-paper'
- 
- class HomePage extends React.Component {
- 
-   render() {
-     return (
-       <View>
-         <Portal>
-           <Appbar.Header>
-             <Appbar.Content title="Counter" />
-           </Appbar.Header>
-           <View>
-           </View>
-           <FAB
-             style={styles.fab}
-             icon="plus"
-             color="white"
-             onPress={() => {}} />
-         </Portal>
-       </View>
-     )
-   }
- }
- 
- const App = () => {
-   return (
-     <PaperProvider>
-       <HomePage />
-     </PaperProvider>
-   );
- };
- 
- const styles = StyleSheet.create({
-   center: {
-     alignItems: 'center',
-     justifyContent: 'center',
-     height: '100%',
-   },
-   fab: {
-     position: 'absolute',
-     margin: 16,
-     right: 0,
-     bottom: 0,
-   },
- })
- 
- export default App;
- 
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+} from 'react-native';
+import {
+  Provider as PaperProvider,
+  Appbar,
+  FAB,
+  Portal,
+} from 'react-native-paper'
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    }
+
+    setInterval(() => {
+      this.setState(prev => ({ counter: prev.counter + 1 }))
+    }, this.props.milliseconds);
+  }
+
+  render() {
+    return (
+      <Text>{this.state.counter}</Text>
+    )
+  }
+}
+
+class HomePage extends React.Component {
+
+  render() {
+    return (
+      <View>
+        <Portal>
+          <Appbar.Header>
+            <Appbar.Content title="Counter" />
+          </Appbar.Header>
+          <View style={styles.center}>
+            <Counter milliseconds={16} />
+          </View>
+        </Portal>
+      </View>
+    )
+  }
+}
+
+const App = () => {
+  return (
+    <PaperProvider>
+      <HomePage />
+    </PaperProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  center: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%',
+  },
+  fab: {
+    position: 'absolute',
+    margin: 16,
+    right: 0,
+    bottom: 0,
+  },
+})
+
+export default App;
