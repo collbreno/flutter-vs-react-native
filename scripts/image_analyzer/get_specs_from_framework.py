7dfe1aa20b67635d36f742ee686241c4a20690af
Breno Coll <collbreno@dcc.ufrj.br>
2022-07-22 22:05:15 -0300

    [Scripts] Changing framework specs from csv to json

--- scripts/image_analyzer/get_specs_from_framework.py
+++ scripts/image_analyzer/get_specs_from_framework.py
@@ -1,4 +1,6 @@
+import json
 import cv2
+import numpy as np
 from frame_analyzer import FrameAnalyzer
 
 def get_input_image_name(framework, suffix):
@@ -7,32 +9,19 @@ def get_input_image_name(framework, suffix):
 def get_output_image_name(framework, suffix):
     return f'./framework_specs/{framework}_{suffix}.png'
 
-def write_line(line):
-        file.write(','.join(line))
-        file.write('\n')
-
 if __name__ == '__main__':
 
-    header = ['framework', 'all_blank', 'blank_item']
-    lines = []
+    obj = {}
     for framework in ['rn', 'flutter']:
-        line = [framework]
+        obj[framework] = {}
         for spec_type in ['all_blank', 'blank_item']:
-
             img = get_input_image_name(framework, spec_type)
-
             analyzer = FrameAnalyzer(img)
-
             analyzer.save_masked(
                 get_output_image_name(framework, spec_type)
             )
-
             white_percentage = analyzer.get_white_percentage()
-
-            line.append(str(white_percentage))
-        lines.append(line)
+            obj[framework][spec_type] = np.round(white_percentage, 2)
     
-    with open('./framework_specs/output.csv', 'w') as file:
-        write_line(header)
-        for line in lines:
-            write_line(line)
+    f = open('framework_specs/output.json', 'w')
+    json.dump(obj, f)
